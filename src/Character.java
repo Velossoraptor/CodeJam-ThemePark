c1043b4fb4eb7b70111d04348846965d74e33bbb
Velossoraptor <130588823+Velossoraptor@users.noreply.github.com>
2023-05-01 14:35:25 -0400

    Added Stuff to Character
    added some methods

--- src/Character.java
+++ src/Character.java
@@ -1,27 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Character {
+public class Character implements KeyListener{
     private static ImageIcon sprite = new ImageIcon("src/sprite.png");
     private String name;
     private int coins = 0;
+    private int lives = 3;
     private int x, y;
 
-    public Character(String newName, JFrame currentPanel){
+    public Character(String newName, JPanel currentPanel){
         this.name=newName;
         currentPanel.add(this);
     }
 
+    //Change Methods
     public void move(int xChange, int yChange){
         this.x+=xChange;
         this.y+=yChange;
     }
 
+    public void loseLife(){
+        this.lives--;
+    }
+
+    public void gainCoin(){
+        this.coins++;
+    }
+
+    //Getter Methods
     public String getName(){
         return this.name;
     }
 
     public int getCoins(){
         return this.coins;
+    }
+
+    public int getLives(){
+        return this.lives;
     }
 }
